655d346f0d3c61021c244deaf1a79285908c62c0
Muhammad-Abid11 <abidkhan11845@gmail.com>
2023-07-26 17:23:03 +0500

    Osmodule path modules add

---
+++ osmodules.js
@@ -0,0 +1,7 @@
+
+const os = require("os");
+console.log(os.arch())
+console.log(os.hostname())
+console.log(os.platform())
+const free = os.freemem();
+console.log(free / 1024 / 1024 / 1024);
---
+++ path.js
@@ -0,0 +1,4 @@
+const path = require("path");
+
+console.log(path.dirname("C:/Users/Home/Desktop/Node-JS"))
+console.log(path.extname("C:/Users/Home/Desktop/Node-JS"))
