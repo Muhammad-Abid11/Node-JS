46173c88e507f3eab730501680cfed6ba7533111
Muhammad-Abid11 <abidkhan11845@gmail.com>
2023-08-01 17:07:12 +0500

    StreamsPipe

---
+++ 22.StreamsPipe/index.js
@@ -0,0 +1,14 @@
+// video 22
+
+// video 21 me humne kafi long method dekha tha " stream " k yahn wo direct
+// " pipe "k through access ho ja rha hai. 
+const fs = require("fs")
+const http = require("http");
+const StreamData = fs.createReadStream("input.txt")//jis file ki stream krne hai uska name
+
+const server = http.createServer((req, res) => {
+    StreamData.pipe(res); //pipe k through " StreamData " k data " res " me send krdo
+})
+server.listen(8000, "localhost", () => {
+    console.log("server is up on port 8000")
+})
